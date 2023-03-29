fixed a bug with .all() in initial field; tested two water spheres; tested (but not enough clear) two steel spheres; deleted garbage from wvfs and mths; hankel -> spherical_h1n (derivative)

acsmuthi/initial_field.py
@@ -28,7 +28,7 @@ class PlaneWave(InitialField):
 
     def spherical_wave_expansion(self, origin, order):
         base_coefficients = wvfs.incident_coefficients(self.dir, order)
-        if origin.all() == self.reference_point.all():
+        if np.array_equal(origin, self.reference_point):
             coefficients = base_coefficients
         else:
             coefficients = cmt.translation_block(order, self.k_l, origin - self.reference_point) @ base_coefficients

acsmuthi/linear_system/t_matrix.py
@@ -19,9 +19,9 @@ def scaled_coefficient(n, c_medium, rho_medium, c_l, rho, r, freq):
     s1[1, 0] = ss.spherical_jn(n, k_medium * r, derivative=True)
     s1[1, 1] = ss.spherical_jn(n, k_l * r, derivative=True)
 
-    s2[0, 0] = - gamma * mths.sph_hankel1(n, k_medium * r)
+    s2[0, 0] = - gamma * mths.spherical_h1n(n, k_medium * r)
     s2[0, 1] = ss.spherical_jn(n, k_l * r)
-    s2[1, 0] = - mths.sph_hankel1_der(n, k_medium * r)
+    s2[1, 0] = - mths.spherical_h1n(n, k_medium * r, derivative=True)
     s2[1, 1] = ss.spherical_jn(n, k_l * r, derivative=True)
 
     return np.linalg.det(s1) / np.linalg.det(s2)
@@ -35,17 +35,18 @@ def elastic_scaled_coefficient(n, c_medium, rho_medium, c_l, c_t, rho, r, freq):
     sigma = (c_l**2 / 2 - c_t**2) / (c_l**2 - c_t**2)
 
     alpha_n = ss.spherical_jn(n, k_l * r) - k_l * r * ss.spherical_jn(n, k_l * r, derivative=True)
-    beta_n = (n**2 + n - 2) * ss.spherical_jn(n, k_t * r) + k_t ** 2 * r ** 2 ** mths.sph_bessel_der2(n, k_t * r)
+    beta_n = (n**2 + n - 2) * ss.spherical_jn(n, k_t * r) + k_t ** 2 * r ** 2 ** mths.spherical_jn_der2(n, k_t * r)
     xi_n = k_l * r * ss.spherical_jn(n, k_l * r, derivative=True)
     delta_n = 2 * n * (n + 1) * ss.spherical_jn(n, k_t * r)
-    epsilon_n = k_l ** 2 * r ** 2 * (ss.spherical_jn(n, k_l * r) * sigma/(1-2*sigma) - mths.sph_bessel_der2(n, k_l * r))
+    epsilon_n = k_l ** 2 * r ** 2 * (ss.spherical_jn(n, k_l * r) * sigma / (1-2*sigma) - mths.spherical_jn_der2(n,
+                                                                                                                k_l * r))
     eta_n = 2 * n * (n + 1) * (ss.spherical_jn(n, k_t * r) - k_t * r * ss.spherical_jn(n, k_t * r, derivative=True))
 
     coefficient = rho_medium * k_t ** 2 * r ** 2 / 2 / rho
     g_n = coefficient * (alpha_n * delta_n + beta_n * xi_n) / (alpha_n * eta_n + beta_n * epsilon_n)
 
     scale = - (g_n * ss.spherical_jn(n, k_medium * r) - k_medium * r * ss.spherical_jn(n, k_medium * r, derivative=True)) / \
-              (g_n * mths.sph_hankel1(n, k_medium * r) - k_medium * r * mths.sph_hankel1_der(n, k_medium * r))
+              (g_n * mths.spherical_h1n(n, k_medium * r) - k_medium * r * mths.spherical_h1n(n, k_medium * r, derivative=True))
     return scale
 
 

acsmuthi/utility/mathematics.py
@@ -5,40 +5,21 @@ import scipy
 import scipy.special as ss
 
 
-def sph_neyman(n, z):
-    r"""Spherical Neyman function"""
-    return (-1) ** (n + 1) * np.sqrt(np.pi / 2 / z) * scipy.special.jv(-n - 0.5, z)
-
-
-def sph_neyman_der(n, z):
-    r"""First derivative of spherical Neyman function"""
-    return (-1) ** n * np.sqrt(np.pi / (8 * z ** 3)) * scipy.special.jv(-n - 0.5, z) + \
-           (-1) ** (n + 1) * np.sqrt(np.pi / 2 / z) * scipy.special.jvp(-n - 0.5, z)
-
-
-def sph_hankel1(n, z):
+def spherical_h1n(n, z, derivative=False):
     r"""Spherical Hankel function of the first kind"""
-    return scipy.special.spherical_jn(n, z) + 1j * sph_neyman(n, z)
+    if derivative:
+        return ss.spherical_jn(n, z, derivative=True) + 1j * ss.spherical_yn(n, z, derivative=True)
+    else:
+        return scipy.special.spherical_jn(n, z) + 1j * scipy.special.spherical_yn(n, z)
 
 
-def sph_hankel1_der(n, z):
-    r"""First derivative of spherical Hankel function of the first kind"""
-    return scipy.special.spherical_jn(n, z, derivative=True) + 1j * sph_neyman_der(n, z)
-
-
-def sph_bessel_der2(n, z):
+def spherical_jn_der2(n, z):
     r"""Second derivative of spherical Bessel function"""
     if n == 0:
         return - ss.spherical_jn(1, z, derivative=True)
     else:
         return ss.spherical_jn(n - 1, z, derivative=True) + (n + 1) / z**2 * ss.spherical_jn(n, z) - \
                (n + 1) / z * ss.spherical_jn(n, z, derivative=True)
-
-
-def csph_harm(m, n, phi, theta):
-    r"""Spherical harmonic of complex argument"""
-    coefficient = np.sqrt((2 * n + 1) / 4 / np.pi * scipy.special.factorial(n - m) / scipy.special.factorial(n + m))
-    return coefficient * np.exp(1j * m * phi) * scipy.special.clpmn(m, n, np.cos(theta), type=2)[0][-1][-1]
 
 
 def dec_to_sph(x, y, z):

acsmuthi/utility/wavefunctions.py
@@ -49,12 +49,6 @@ def regular_wvf(m, n, x, y, z, k):
     return ss.spherical_jn(n, k * r) * scipy.special.sph_harm(m, n, phi, theta)
 
 
-def cregular_wvf(m, n, x, y, z, k):
-    r"""Regular basis spherical wave function of complex argument"""
-    r, phi, theta = mths.dec_to_sph(x, y, z)
-    return ss.spherical_jn(n, k * r) * mths.csph_harm(m, n, phi, theta)
-
-
 def regular_wvfs_array(order, x, y, z, k):
     r"""Builds np.array of all regular wave functions with n <= order"""
     rw_array = np.zeros(((order + 1) ** 2, len(x)), dtype=complex)
@@ -68,13 +62,7 @@ def regular_wvfs_array(order, x, y, z, k):
 def outgoing_wvf(m, n, x, y, z, k):
     r"""Outgoing basis spherical wave function"""
     r, phi, theta = mths.dec_to_sph(x, y, z)
-    return mths.sph_hankel1(n, k * r) * ss.sph_harm(m, n, phi, theta)
-
-
-def coutgoing_wvf(m, n, x, y, z, k):
-    r"""Outgoing basis spherical wave function"""
-    r, phi, theta = mths.dec_to_sph(x, y, z)
-    return mths.sph_hankel1(n, k * r) * mths.csph_harm(m, n, phi, theta)
+    return mths.spherical_h1n(n, k * r) * ss.sph_harm(m, n, phi, theta)
 
 
 def outgoing_wvfs_array(order, x, y, z, k):
@@ -87,30 +75,6 @@ def outgoing_wvfs_array(order, x, y, z, k):
     return ow_array
 
 
-def local_incident_coefficient(m, n, k, direction, position, order):
-    r"""Counts local incident coefficients"""
-    incident_coefficient_array = np.zeros((order+1) ** 2, dtype=complex)
-    for mu, nu in multipoles(order):
-        i = nu ** 2 + nu + mu
-        incident_coefficient_array[i] = incident_coefficient(mu, nu, direction) * \
-                                        regular_separation_coefficient(mu, m, nu, n, k, position)
-    return mths.complex_fsum(incident_coefficient_array)
-
-
-def axisymmetric_outgoing_wvf(n, x, y, z, k):
-    r"""Outgoing axisymmetric basis spherical wave function"""
-    r, phi, theta = mths.dec_to_sph(x, y, z)
-    return mths.sph_hankel1(n, k * r) * ss.lpmv(0, n, np.cos(theta))
-
-
-def axisymmetric_outgoing_wvfs_array(x, y, z, k, length, order):
-    r"""Builds np.array of all axisymmetric outgoing wave functions with n <= order"""
-    as_ow_array = np.zeros((order + 1, length), dtype=complex)
-    for n in range(order + 1):
-        as_ow_array[n] = axisymmetric_outgoing_wvf(n, x, y, z, k)
-    return as_ow_array
-
-
 def regular_separation_coefficient(m, mu, n, nu, k, dist):
     r"""Coefficient ^S^mmu_nnu(b) of separation matrix"""
     if abs(n - nu) >= abs(m - mu):
@@ -121,10 +85,7 @@ def regular_separation_coefficient(m, mu, n, nu, k, dist):
         q0 = abs(m - mu) + 1
     q_lim = (n + nu - q0) // 2
     sum_array = np.zeros(q_lim + 1, dtype=complex)
-    if dist.dtype == complex:
-        reg_wvf = cregular_wvf
-    else:
-        reg_wvf = regular_wvf
+    reg_wvf = regular_wvf
     i = 0
     for q in range(0, q_lim + 1):
         sum_array[i] = (-1) ** q * reg_wvf(m - mu, q0 + 2 * q, dist[0], dist[1], dist[2], k) * \
@@ -143,10 +104,7 @@ def outgoing_separation_coefficient(m, mu, n, nu, k, dist):
         q0 = abs(m - mu) + 1
     q_lim = (n + nu - q0) // 2
     sum_array = np.zeros(q_lim + 1, dtype=complex)
-    if dist.dtype == complex:
-        out_wvf = coutgoing_wvf
-    else:
-        out_wvf = outgoing_wvf
+    out_wvf = outgoing_wvf
     i = 0
     for q in range(0, q_lim + 1):
         sum_array[i] = (-1) ** q * out_wvf(m - mu, q0 + 2 * q, dist[0], dist[1], dist[2], k) * \

tests/total/one_sphere_test.py
@@ -15,6 +15,19 @@ comparison: solution using package and exact solution using spherical expansion
 source: Acoustic force and torque on small objects. Notes. Ivan Toftul
 '''
 
+def axisymmetric_outgoing_wvf(n, x, y, z, k):
+    r"""Outgoing axisymmetric basis spherical wave function"""
+    r, phi, theta = mths.dec_to_sph(x, y, z)
+    return mths.spherical_h1n(n, k * r) * scipy.special.lpmv(0, n, np.cos(theta))
+
+
+def axisymmetric_outgoing_wvfs_array(x, y, z, k, length, order):
+    r"""Builds np.array of all axisymmetric outgoing wave functions with n <= order"""
+    as_ow_array = np.zeros((order + 1, length), dtype=complex)
+    for n in range(order + 1):
+        as_ow_array[n] = axisymmetric_outgoing_wvf(n, x, y, z, k)
+    return as_ow_array
+
 
 def pn_coefficient_1s(n):
     r"""
@@ -40,9 +53,9 @@ def desired_scattered_coefficient_1s(n, k_s, r_s, rho_s, k, rho):
     a_n = (gamma * scipy.special.spherical_jn(n, k_s * r_s, derivative=True) *
            scipy.special.spherical_jn(n, k * r_s) - scipy.special.spherical_jn(n, k_s * r_s) *
            scipy.special.spherical_jn(n, k * r_s, derivative=True)) / \
-          (scipy.special.spherical_jn(n, k_s * r_s) * mths.sph_hankel1_der(n, k * r_s) -
+          (scipy.special.spherical_jn(n, k_s * r_s) * mths.spherical_h1n(n, k * r_s, derivative=True) -
            gamma * scipy.special.spherical_jn(n, k_s * r_s, derivative=True) *
-           mths.sph_hankel1(n, k * r_s))
+           mths.spherical_h1n(n, k * r_s))
     return a_n
 
 
@@ -52,9 +65,9 @@ def desired_in_coefficient_1s(n, k_s, r_s, rho_s, k, rho):
     in basis of functions :math:`j_n(kr) P_n(\cos\theta)`
     """
     gamma = k_s * rho / k / rho_s
-    c_n = 1j / (k * r_s) ** 2 / (scipy.special.spherical_jn(n, k_s * r_s) * mths.sph_hankel1_der(n, k * r_s) -
+    c_n = 1j / (k * r_s) ** 2 / (scipy.special.spherical_jn(n, k_s * r_s) * mths.spherical_h1n(n, k * r_s, derivative=True) -
                                  gamma * scipy.special.spherical_jn(n, k_s * r_s, derivative=True) *
-                                 mths.sph_hankel1(n, k * r_s))
+                                 mths.spherical_h1n(n, k * r_s))
     return c_n
 
 
@@ -74,7 +87,7 @@ def scattered_field_1s(x, y, z, k_s, r_s, rho_s, k, rho, order):
     calculates scattered field in every point
     """
     tot_field_array = desired_pscattered_coefficients_array_1s(k_s, r_s, rho_s, k, rho, len(x), order) * \
-                      wvfs.axisymmetric_outgoing_wvfs_array(x, y, z, k, len(x), order)
+                      axisymmetric_outgoing_wvfs_array(x, y, z, k, len(x), order)
     return np.real(np.sum(tot_field_array, axis=0))
 
 
@@ -95,7 +108,7 @@ def cross_sections_1s(k_s, r_s, rho_s, k, rho, order):
     return sigma_sc, sigma_ex
 
 
-def one_sphere_test1():
+def test_one_sphere():
     # parameters of medium
     ro_fluid = 1.225  # [kg/m^3]
     c_fluid = 331  # [m/s]
@@ -168,14 +181,11 @@ def one_sphere_test1():
     desired_field = np.where(r <= sphere1.radius, 0, desired_field)
 
     err = np.abs((actual_field - desired_field))
-    rendering.slice_plot(err, span_v, span_h, plane=plane)
-    rendering.plots_for_tests(actual_field, desired_field, span_v, span_h)
+    # rendering.slice_plot(err, span_v, span_h, plane=plane)
+    # rendering.plots_for_tests(actual_field, desired_field, span_v, span_h)
 
     desired_scs, desired_ecs = cross_sections_1s(2 * np.pi * freq / c_sph, r_sph, ro_sph, k_l, ro_fluid, order)
 
     np.testing.assert_allclose(actual_field, desired_field, rtol=1e-5)
     np.testing.assert_allclose(desired_ecs, ecs)
-    np.testing.assert_allclose(desired_scs, scs)
-
-
-one_sphere_test1()
+    np.testing.assert_allclose(desired_scs, scs / (np.pi * r_sph ** 2))

tests/total/two_steel_spheres_in_water.py
@@ -26,7 +26,7 @@ def test_two_steel_spheres_in_water():
     pos1 = np.array([-1.6 * r_sph, 0, 0])  # [m]
     pos2 = np.array([1.6 * r_sph, 0, 0])  # [m]
 
-    order = 8
+    order = 10
 
     incident_field = PlaneWave(k_l=k_l,
                                amplitude=p0,
@@ -68,8 +68,10 @@ def test_two_steel_spheres_in_water():
                              medium=fluid,
                              incident_field=incident_field)
 
-    comsol_scs = 6.8682E-4  # 6.9386E-4
-    comsol_frcs = np.array([[4.5201E-14, -2.2189E-17, 5.1318E-14], [5.1488E-14,	1.3965E-17,	5.9230E-14]])
+    # comsol_scs = 6.868E-4  # 6.9386E-4
+    # comsol_frcs = np.array([[4.5201E-14, 0, 5.1318E-14], [5.1488E-14, 0, 5.9230E-14]])
+    comsol_scs = 6.9092E-4  # 6.8824E-4
+    comsol_frcs = np.array([[4.5198E-14, 0, 5.1185E-14], [5.1514E-14, 0, 5.9213E-14]])
 
-    # np.testing.assert_allclose(frcs, comsol_frcs, rtol=1e-1)
-    assert scs == 9.5773E-4  # 9.6687E-4
+    np.testing.assert_allclose(np.where(np.abs(frcs) <= 1e-14, 0, frcs), comsol_frcs, rtol=5e-1)
+    assert np.round(scs, 4) == np.round(comsol_scs, 4)

tests/total/two_water_spheres_in_air.py
@@ -27,7 +27,7 @@ def test_two_water_spheres_in_air():
     pos1 = np.array([-2.5, 0, 0])  # [m]
     pos2 = np.array([2.5, 0, 0])  # [m]
 
-    order = 12
+    order = 9
 
     incident_field = PlaneWave(k_l=k_l,
                                amplitude=p0,
@@ -66,10 +66,10 @@ def test_two_water_spheres_in_air():
                              medium=fluid,
                              incident_field=incident_field)
 
-    # comsol_scs = 3.9407  # 3.9369
     # comsol_frcs = np.array([[6.7778E-6, -9.7296E-9, 5.8066E-6], [5.5507E-6, 9.0569E-9, 6.0051E-6]])
     comsol_scs = 3.9354  # 3.9339
-    comsol_frcs = np.array([[6.8436E-6, - 4.8389E-10, 5.8481E-6], [5.5495E-6, 1.1734E-10, 5.9831E-6]])
+    comsol_frcs = np.array([[6.8436E-6, 0, 5.8481E-6], [5.5495E-6, 0, 5.9831E-6]])
 
-    # np.testing.assert_allclose(frcs, comsol_frcs, rtol=1e-1)
-    assert scs == comsol_scs
+    np.testing.assert_allclose(np.where(np.abs(frcs) <= 1e-14, 0, frcs), comsol_frcs, rtol=1e-2)
+    assert np.round(scs, 2) == np.round(comsol_scs, 2)
+