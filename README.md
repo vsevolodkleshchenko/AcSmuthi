8d4fde880ed3f13096d0f2eeb4d18e3469df22b6
Vsevolod Kleshchenko <k.vd@icloud.com>
2023-05-17 23:54:03 +0300

    some bugs

--- README.md
+++ README.md
@@ -12,24 +12,19 @@ poetry run python run.py
 ```
 
 
-## How it works?
-Before getting the results the `Simulation` must be created, that means
-to define the `Particle`s array, `Medium`, `Initial field`, it's frequency, choose the multipole order and specify the plot 
-parameters (if necessary). 
-
-Run the created simulation to get all the results with parameters:
-```
-simulation.run(cross_sections_flag=True, forces_flag=True, plot_flag=True)
-```
-
-
 ## What can it do?
 * Compute and render pressure near-fields
 * Calculate scattering and extinction cross-sections
 * Calculate forces acting on particles
 
+
+## Installation
+Now, package is not published on pypi and available only with cloning from github 
+
+
 ## What realized now?
-* Plane wave scattering on spherical fluid and elastic particles in fluid medium
+* Plane wave scattering on compressible spherical particles in fluid medium
+
 
 ## Acknowledgements
 This work is supported by the Russian Science Foundation # 20-72-10141
--- run.py
+++ run.py
@@ -7,17 +7,23 @@ from acsmuthi.postprocessing import rendering
 
 import numpy as np
 
-
+# parameters of surrounded medium
 rho_fluid, c_fluid = 1.225, 331
+# parameters of acoustic field (plane wave)
 p0, freq = 1, 82
 direction = np.array([0.70711, 0, 0.70711])
 k = 2 * np.pi * freq / c_fluid
+# parameters of particles
 r_sph, rho_sph, c_sph = 1., 997, 1403
 
+# order of multipole expansion
 order = 8
 
+# creating acoustic field
 incident_field = PlaneWave(k=k, amplitude=p0, direction=direction)
+# creating surrounded medium
 medium = Medium(density=rho_fluid, pressure_velocity=c_fluid)
+# creating 3 spherical particles
 sphere1 = SphericalParticle(
     position=np.array([-2., 0, 3]),
     radius=r_sph,
@@ -40,6 +46,7 @@ sphere3 = SphericalParticle(
     order=order
 )
 particles = np.array([sphere1, sphere2, sphere3])
+# creating simulation object
 simulation = Simulation(
     particles=particles,
     medium=medium,
@@ -48,10 +55,13 @@ simulation = Simulation(
     order=order
 )
 
+# simulation.run() method returns preparation and solving matrix system time
 print("Time:", simulation.run())
 
+# computing extinction cross-section
 ecs = cs.extinction_cs(simulation, by_multipoles=False)
+# computing forces
 frcs = forces.all_forces(simulation)
 print(ecs, *frcs, sep='\n')
-
+# easy way to draw total field (also it's possible to show only 'scattered' or 'incident' field)
 rendering.show_pressure_field(simulation, -6, 6, 0, 0, -6, 6, 151, 'total')
