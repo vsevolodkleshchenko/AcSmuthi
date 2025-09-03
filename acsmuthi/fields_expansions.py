50db7282bc9a7f36bf3fb5d4f413157bc8dccc07
Vsevolod Kleshchenko <k.vd@icloud.com>
2025-09-03 22:37:20 +0300

    pwe, plane wave rewritten

--- acsmuthi/fields_expansions.py
+++ acsmuthi/fields_expansions.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import Literal
+from typing import Literal, Self
 
 import numpy as np
 
-from acsmuthi.utility.wavefunctions import mn_idx, outgoing_wvf, regular_wvf
+import acsmuthi.fields_expansions as fldsex
+from acsmuthi.utility.wavefunctions import mn_idx, outgoing_wvf, regular_wvf, plane_wave_sfe_cfs
 
 
 class FieldExpansion(ABC):
@@ -43,7 +44,7 @@ class FieldExpansion(ABC):
         pass
 
 
-class SphericalWaveExpansion(FieldExpansion):
+class SphericalWaveExpansion(FieldExpansion):  # todo: remove amplitude
     """Representation of field in spherical waves (multipole) expansion.
 
     :math:`\\sum_{n,m} c^n_m z_n(kr) Y^m_n(\\vec{r})`, where :math:`c^m_n` are the expansion coefficients, and
@@ -51,16 +52,16 @@ class SphericalWaveExpansion(FieldExpansion):
 
     """
     def __init__(
-            self,
-            amplitude: float,
-            k: float,
-            reference_point: np.ndarray,
-            kind: Literal['regular', 'outgoing'],
-            n_max: int,
-            inner_r: float = 0,
-            outer_r: float = np.inf,
-            lower_z: float = - np.inf,
-            coefficients: np.ndarray = None
+        self,
+        amplitude: float,
+        k: float,
+        reference_point: np.ndarray,
+        kind: Literal['regular', 'outgoing'],
+        n_max: int,
+        inner_r: float = 0,
+        outer_r: float = np.inf,
+        lower_z: float = - np.inf,
+        coefficients: np.ndarray = None
     ):
         """Spherical field expansion constructor.
 
@@ -126,18 +127,121 @@ class SphericalWaveExpansion(FieldExpansion):
         multipole_fields = coefficients * wave_functions
         return self.ampl * np.sum(multipole_fields, axis=0)
 
-    # def compatible(self, other: Self):
-    #     return (type(other).__name__ == "SphericalWaveExpansion"  # todo: maybe it is possible to do it easier
-    #             and self.k == other.k
-    #             and self.n_max == other.n_max
-    #             and self.kind == other.kind
-    #             and np.array_equal(self.reference_point, other.reference_point))
-    #
-    # def __add__(self, other: Self):
-    #     if not self.compatible(other):
-    #         raise ValueError('SphericalWaveExpansions are inconsistent.')
-    #     swe_sum = SphericalWaveExpansion(amplitude=self.ampl, k=self.k, reference_point=self.reference_point,
-    #                                      kind=self.kind, n_max=self.n_max, inner_r=max(self.inner_r, other.inner_r),
-    #                                      outer_r=min(self.outer_r, other.outer_r))
-    #     swe_sum.coefficients = self.coefficients + other.coefficients
-    #     return swe_sum
+    def compatible(self, other: Self):
+        return (type(other).__name__ == "SphericalWaveExpansion"  # todo: maybe it is possible to do it easier
+                and self.k == other.k
+                and self.n_max == other.n_max
+                and self.kind == other.kind
+                and np.array_equal(self.reference_point, other.reference_point)
+                and self.lower_z == other.lower_z)
+
+    def __add__(self, other: Self) -> Self:
+        if not self.compatible(other):
+            raise ValueError('SphericalWaveExpansions are inconsistent.')
+        swe_sum = SphericalWaveExpansion(
+            amplitude=self.ampl,
+            k=self.k,
+            reference_point=self.reference_point,
+            kind=self.kind,
+            n_max=self.n_max,
+            inner_r=max(self.inner_r, other.inner_r),
+            outer_r=min(self.outer_r, other.outer_r),
+            lower_z=self.lower_z,
+        )
+        swe_sum.coefficients = self.coefficients + other.coefficients
+        return swe_sum
+
+
+class PlaneWaveExpansion(FieldExpansion):
+    """Representation of field in plane wave expansion. Currently only a single plane wave is supported.
+
+    :math:`p(\\vec{r}) = A e^{i \\vec{k} \\cdot (\\vec{r}-\\vec{r_0})}`, where
+    :math:`A` is the amplitude, :math:`\\vec{k}` is the wave vector, and :math:`\\vec{r_0}` is the reference point.
+    """
+
+    def __init__(
+        self,
+        k_vector: np.ndarray,
+        amplitude: float,
+        lower_z: float = - np.inf,
+        upper_z: float = np.inf,
+        reference_point: np.ndarray | None = None
+    ):
+        """Plane wave field expansion constructor.
+
+        Args:
+            k_vector (3d array): k vector of the plane wave
+            amplitude: amplitude of the plane wave in Pa
+            lower_z (float, optional): minimal z-coordinate value where the expansion is valid. Defaults to -np.inf.
+            upper_z (float, optional): maximum z-coordinate value where the expansion is valid. Defaults to np.inf.
+            reference_point: coordinates of point relative to which the plane wave is considered.
+        """
+        super().__init__()
+        self.k_vec = k_vector
+        self.ampl = amplitude
+        if reference_point is None:
+            reference_point = np.zeros(3)
+        self.reference_point = reference_point
+        self.lower_z = lower_z
+        self.upper_z = upper_z
+
+    @property
+    def k(self) -> float:
+        return np.linalg.norm(self.k_vec)
+
+    @property
+    def kind(self) -> Literal['upgoing', 'downgoing']:
+        if self.k_vec[2] >= 0:
+            return 'upgoing'
+        else:
+            return 'downgoing'
+
+    @property
+    def k_parallel(self) -> float:
+        return np.sqrt(self.k_vec[0]**2 + self.k_vec[1]**2)
+
+    def valid(self, x: np.ndarray, y: np.ndarray, z: np.ndarray):
+        """Test if points are in definition range of the expansion.
+
+        :param x: x-coordinates of query points
+        :param y: y-coordinates of query points
+        :param z: z-coordinates of query points
+        :return: array indicating if points are inside definition domain
+        """
+        return np.logical_and(z >= self.lower_z, z <= self.upper_z)
+
+    def pressure_field(self, x: np.ndarray, y: np.ndarray, z: np.ndarray) -> np.ndarray:
+        """Evaluate complex pressure field of a plane wave of the shape of x.
+        """
+        xr = x - self.reference_point[0]
+        yr = y - self.reference_point[1]
+        zr = z - self.reference_point[2]
+        kr = self.k_vec[0] * xr + self.k_vec[1] * yr + self.k_vec[2] * zr
+
+        vld = self.valid(x, y, z)
+        p = np.zeros(x.shape, dtype=complex)
+        p[vld] = self.ampl * np.exp(1j * kr[vld])
+        return p
+
+
+def pwe_to_swe(
+    pwe: fldsex.PlaneWaveExpansion,
+    reference_point: np.ndarray,
+    n_max: int
+) -> fldsex.SphericalWaveExpansion:
+    """Transform plane wave expansion to spherical wave expansion with respect to a given reference point.
+    """
+    pw_direction = pwe.k_vec / pwe.k
+    pw_coefs = plane_wave_sfe_cfs(direction=pw_direction, n_max=n_max)
+    kr_transl = pwe.k_vec @ (reference_point - pwe.reference_point)
+    return fldsex.SphericalWaveExpansion(
+        amplitude=pwe.ampl,
+        k=pwe.k,
+        reference_point=reference_point,
+        kind='regular',
+        n_max=n_max,
+        inner_r=0,
+        outer_r=np.inf,  # todo: check it,
+        lower_z=pwe.lower_z,
+        coefficients=pw_coefs * np.exp(1j * kr_transl)
+    )
--- acsmuthi/initial_field.py
+++ acsmuthi/initial_field.py
@@ -2,11 +2,11 @@ from abc import ABC, abstractmethod
 import numpy as np
 
 import acsmuthi.fields_expansions as fldsex
-import acsmuthi.utility.wavefunctions as wvfs
+from acsmuthi.fields_expansions import pwe_to_swe
 from acsmuthi.medium import MediumSystem
 
 
-class InitialField(ABC):     # todo: frequency, not k; validity conditions
+class InitialField(ABC):
     """Abstract class for initial (incident) fields.
     """
     def __init__(self, frequency: float, amplitude: float):
@@ -57,11 +57,41 @@ class PlaneWave(InitialField):
         InitialField.__init__(self, frequency=frequency, amplitude=amplitude)
         self.direction = direction
         if reference_point is None:
-            self.reference_point = np.array([0, 0, 0])
+            self.reference_point = np.zeros(3)
         else:
             self.reference_point = reference_point
 
-    def spherical_wave_expansion(   # todo: think about args; transfer pwe to sfe????
+    def plane_wave_expansion(
+        self,
+        medium: MediumSystem
+    ) -> tuple[fldsex.PlaneWaveExpansion]:
+        """Compute plane wave expansion of the plane wave pressure field.
+
+        Args:
+            medium: medium system where the field expansion is computed
+        """
+        k = medium.sur_medium.wavenumber(self.freq)
+        k_vector = k * self.direction
+        incident_pwe = fldsex.PlaneWaveExpansion(
+            k_vector=k_vector,
+            amplitude=self.amplitude,
+            reference_point=self.reference_point,
+            lower_z=0 if medium.is_substrate and self.direction[2] < 0 else -np.inf,
+            upper_z=np.inf
+        )
+
+        if medium.is_substrate and self.direction[2] < 0:
+            reflected_pwe = reflect(
+                pwe=incident_pwe,
+                medium=medium,
+                freq=self.freq
+            )
+            return incident_pwe, reflected_pwe
+
+        else:
+            return tuple([incident_pwe])
+
+    def spherical_wave_expansion(   # todo: think about args
         self,
         reference_point: np.ndarray,
         medium: MediumSystem,
@@ -74,37 +104,23 @@ class PlaneWave(InitialField):
             order (int): multipole order of spherical expansion
             reference_point (np.ndarray): reference point for the spherical wave expansion of the plane wave
         """
-        k = medium.sur_medium.wavenumber(self.freq)
-        reference_coefficients = wvfs.plane_wave_sfe_cfs(self.direction, order)
+        sw_expansions = []
+        for pwe in self.plane_wave_expansion(medium=medium):
+            swe = pwe_to_swe(pwe=pwe, reference_point=reference_point, n_max=order)
+            sw_expansions.append(swe)
 
-        if np.array_equal(reference_point, self.reference_point):
-            coefficients = reference_coefficients
-        else:
-            kr = k * self.direction @ (reference_point - self.reference_point)
-            coefficients = np.exp(1j * kr) * reference_coefficients
-
+        # extremely harcoded - todo: fix!!!!!!
         if medium.is_substrate and self.direction[2] < 0:
-            reflection_phase = np.exp(-2j * self.direction[2] * k * self.reference_point[2])
-            reflected_direction = np.array([self.direction[0], self.direction[1], -self.direction[2]])
-            r = medium.fresnel_r(k_parallel=k * np.linalg.norm(self.direction[:-1]), frequency=self.freq)
-            reflected_pw_cfs = wvfs.plane_wave_sfe_cfs(direction=reflected_direction, n_max=order)
-            reflected_coefficients = r * reflection_phase * reflected_pw_cfs
-            if not np.array_equal(reference_point, self.reference_point):
-                kr = k * reflected_direction @ (reference_point - self.reference_point)
-                reflected_coefficients *= np.exp(1j * kr)
+            swe_inc, swe_ref = sw_expansions
+            ampl = swe_inc.ampl
+            r_eikz_factor = swe_ref.ampl / ampl
+            swe_ref.coefficients *= r_eikz_factor
+            swe_ref.ampl = ampl
+            return swe_inc + swe_ref
+        else:
+            return sw_expansions[0]
 
-            coefficients += reflected_coefficients
-
-        return fldsex.SphericalWaveExpansion(
-            amplitude=self.amplitude,
-            k=k,
-            reference_point=reference_point,
-            kind='regular',
-            n_max=order,
-            coefficients=coefficients
-        )
-
-    def pressure_field(  # todo: through the pfe
+    def pressure_field(
         self,
         x: float | np.ndarray,
         y: float | np.ndarray,
@@ -113,68 +129,33 @@ class PlaneWave(InitialField):
     ) -> np.ndarray:
         """Compute the exact pressure field of the plane wave in medium in given coordinates.
         """
-        k = medium.sur_medium.wavenumber(self.freq)
-        exact_field = self.amplitude * np.exp(
-            1j * k * (
-                self.direction[0] * (x - self.reference_point[0]) +
-                self.direction[1] * (y - self.reference_point[1]) +
-                self.direction[2] * (z - self.reference_point[2])
-            )
-        )
-        if medium.is_substrate:
-            r = medium.fresnel_r(k_parallel=k * np.linalg.norm(self.direction[:-1]), frequency=self.freq)
-
-            if self.direction[2] < 0:
-                exact_field += r * self.amplitude * np.exp(
-                    1j * k * (
-                        self.direction[0] * (x - self.reference_point[0]) +
-                        self.direction[1] * (y - self.reference_point[1]) -
-                        self.direction[2] * (z - self.reference_point[2])
-                    )
-                ) * np.exp(-2j * self.direction[2] * k * self.reference_point[2])
-            exact_field = np.where(z >= 0, exact_field, 0)
+        exact_field = np.zeros_like(x, dtype=complex)
+        for pwe in self.plane_wave_expansion(medium=medium):
+            exact_field += pwe.pressure_field(x=x, y=y, z=z)
         return exact_field
 
     def intensity(self, medium: MediumSystem) -> float:
         return self.amplitude ** 2 / (2 * medium.sur_medium.density * medium.sur_medium.c_longitudinal)
 
 
-# class StandingWave(InitialField):
-# #  todo: it doesn't work - delete or change or do something; maybe make summation method
-#     def __init__(self, k, amplitude, direction, reference_point=None):
-#         InitialField.__init__(self, k=k, amplitude=amplitude)
-#         self.direction = direction
-#         if reference_point is None:
-#             self.reference_point = np.array([0, 0, 0])
-#         else:
-#             self.reference_point = reference_point
+def reflect(
+    pwe: fldsex.PlaneWaveExpansion,
+    medium: MediumSystem,
+    freq: float,
+) -> fldsex.PlaneWaveExpansion:
+    """Reflect downgoing wave defined by plane wave expansion from a substrate.
+    """
+    assert medium.is_substrate, "Medium must contain substrate"
+    assert pwe.kind == 'downgoing', "Only downgoing plane waves can be reflected"
 
-#     def spherical_wave_expansion(self, origin, medium, order):
-#         reference_coefficients_forward = wvfs.plane_wave_sfe_cfs(self.direction, order)
-#         reference_coefficients_backward = wvfs.plane_wave_sfe_cfs(-self.direction, order)
-#         if np.array_equal(origin, self.reference_point):
-#             coefficients = reference_coefficients_forward + reference_coefficients_backward
-#         else:
-#             phase_forward = np.exp(1j * self.k * self.direction @ (origin - self.reference_point))
-#             phase_backward = np.exp(-1j * self.k * self.direction @ (origin - self.reference_point))
-#             coefficients = phase_forward * reference_coefficients_forward + \
-#                            phase_backward * reference_coefficients_backward
-#         return fldsex.SphericalWaveExpansion(amplitude=self.amplitude, k=self.k,
-#                                               reference_point=origin, kind='regular',
-#                                              n_max=order, coefficients=coefficients)
-
-#     def compute_exact_field(self, x, y, z):
-#         exact_field = self.amplitude * np.exp(1j * self.k * (
-#                 self.direction[0] * (x - self.reference_point[0]) +
-#                 self.direction[1] * (y - self.reference_point[1]) +
-#                 self.direction[2] * (z - self.reference_point[2])
-#         ))
-#         exact_field += self.amplitude * np.exp(1j * self.k * (
-#                 -self.direction[0] * (x - self.reference_point[0]) +
-#                 -self.direction[1] * (y - self.reference_point[1]) +
-#                 -self.direction[2] * (z - self.reference_point[2])
-#         ))
-#         return exact_field
-
-#     def intensity(self, density, sound_speed):
-#         return self.amplitude ** 2 / (2 * density * sound_speed)
+    r = medium.fresnel_r(k_parallel=pwe.k_parallel, frequency=freq)
+    kvec_r = np.array([pwe.k_vec[0], pwe.k_vec[1], -pwe.k_vec[2]])
+    phase_r = np.exp(-2j * pwe.k_vec[2] * pwe.reference_point[2])
+    amplitude_r = r * pwe.ampl * phase_r
+    return fldsex.PlaneWaveExpansion(
+        k_vector=kvec_r,
+        amplitude=amplitude_r,
+        reference_point=pwe.reference_point,
+        lower_z=0,
+        upper_z=np.inf
+    )
